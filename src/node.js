f7e50f2742cf7495437e1db01816ae56738bc3f7
unknown <paprotski1702@gmail.com>
2017-10-01 22:20:13 +0300

    1

--- src/node.js
+++ src/node.js
@@ -1,13 +1,35 @@
 class Node {
 	constructor(data, priority) {
+		this.data=data;
+		this.priority=priority;
+		this.left=null;
+		this.right=null;
+		this.parent=null;
 
 	}
 
 	appendChild(node) {
+		if (node.data<this.data) {
+			this.left=node.data;
+		}
+		if (node.data>this.data) {
+            this.right=node.data;
+		}
 
 	}
 
 	removeChild(node) {
+		if(node.data===this.left.data&&
+			node.priority===this.left.priority) {
+			this.left=null;
+		}
+        else if(node.data===this.right.data&&
+            node.priority===this.right.priority) {
+            this.right=null;
+        }
+        else {
+			throw new Error();
+		}
 
 	}
 
