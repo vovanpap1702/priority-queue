65ccca64d55b02346700af5aa1e605154ce6530b
unknown <paprotski1702@gmail.com>
2017-10-04 11:37:25 +0300

    1

--- src/node.js
+++ src/node.js
@@ -19,6 +19,11 @@ class Node {
 		}
 
 	}
+	appendParent(node)
+	{
+
+
+	}
 
 	removeChild(node) {
 		if(node.data===this.left.data&&
@@ -47,17 +52,96 @@ class Node {
 
 	swapWithParent() {
         if(this.parent!==null) {
-        	/*var tmpp=this.parent.parent;
-        	var tmpr=this.parent.right;
-        	var tmpl=this.parent.left;*/
-        	var tmp=this.parent;
-        	this.parent.priority=this.priority;
-        	this.parent.data=this.data;
-        	this.priority=tmp.priority;
-        	this.data=tmp.data;
-        }
+        	const pr=this.right;
+        	const pl=this.left;
+            const p=this.parent;
+           /* const pp=this.parent.parent;
+            const cl=this.left;
+            const cr=this.right;*/
+            this.parent.data=this.data;
+            this.parent.priority=this.priority;
+            this.parent.removeChild(this);
+            //this.parent.left=this.left;//good
+           // this.parent.right=this.right;//good
+            this.parent.appendChild(p);
+            p.left=pr;
+            p.right=pl;
+
+		}
 		
 	}
 }
+const node = new Node(15, 42);
+node.swapWithParent();
 
+const parent = new Node(15, 42);
+const child = new Node(42, 15);
+
+parent.appendChild(child);
+child.swapWithParent();
+
+console.log(parent.parent===child);
+/*
+
+
+it('updates child.parent', () => {
+    const parentOfParent = new Node(100, 500);
+const parent = new Node(15, 42);
+const child = new Node(42, 15);
+
+parentOfParent.appendChild(parent);
+parent.appendChild(child);
+child.swapWithParent();
+
+expect(child.parent).to.equal(parentOfParent);
+});
+
+it('updates parent.child.parent', () => {
+    const root = new Node(1, 2);
+const left = new Node(3, 4);
+const right = new Node(5, 6);
+
+root.appendChild(left);
+root.appendChild(right);
+
+right.swapWithParent();
+
+expect(left.parent).to.equal(right);
+})
+
+it('updates children of node and parent node', () => {
+    const root = new Node(42, 15);
+const left = new Node(13, 42);
+const right = new Node(0, 1);
+const childOfLeft = new Node(0, 15);
+
+root.appendChild(left);
+root.appendChild(right);
+left.appendChild(childOfLeft);
+
+left.swapWithParent();
+
+expect(left.right).to.equal(right);
+expect(left.left).to.equal(root);
+expect(root.left).to.equal(childOfLeft);
+});
+
+const root = new Node(15, 42);
+const left = new Node(42, 15);
+const right = new Node(13, 42);
+const childOfLeft = new Node(13, 34);
+const childOfRight = new Node(0, 1);
+
+root.appendChild(left);
+root.appendChild(right);
+left.appendChild(childOfLeft);
+right.appendChild(childOfRight);
+
+childOfLeft.swapWithParent();
+childOfRight.swapWithParent();
+
+expect(root.left).to.equal(childOfLeft);
+expect(root.right).to.equal(childOfRight);
+
+*/
 module.exports = Node;
