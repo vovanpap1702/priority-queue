600319e034a249a4ff07e2703b415d9164cf4605
unknown <paprotski1702@gmail.com>
2017-10-13 14:45:04 +0300

    1

--- src/max-heap.js
+++ src/max-heap.js
@@ -121,22 +121,83 @@ class MaxHeap {
 
     }
 	shiftNodeDown(node) {
+        /*if (node.parent !== null && node.priority > node.parent.priority) {
+            for (var i = 0; i < this.parentNodes.length; i++) {
+                if (this.parentNodes[i] === node) {
+                    var j = 0;
+                    for (j = 0; j < this.parentNodes.length; j++) {
+                        if (this.parentNodes[j] === node.parent) {
+                            var tmp = this.parentNodes[i];
+                            this.parentNodes[i] = this.parentNodes[j];
+                            this.parentNodes[j] = tmp;
+                            break;
+                        }
+                    }
+                    if(j===this.parentNodes.length) {
+                        this.parentNodes[i] = node.parent;
+                    }
+                }
+            }
+            node.swapWithParent();
+            this.shiftNodeDown(node);
+        }*/
+        if (node.right !== null||node.left!==null) {
+            if (node.right===null) {
+                if(node.left.priority>node.priority) {
+                    node.left.swapWithParent();
+                }
+            }
+            else
+            {
+                if(node.left.priority>node.priority||
+                    node.right.priority>node.priority) {
+                    if(node.left.priority>node.priority&&
+                        node.right.priority>node.priority) {
+                        if(node.left.priority>node.right.priority) {
+                            node.left.swapWithParent();
+                        }
+                        else {
+                            node.right.swapWithParent();
+                        }
+                    }
+                }
+            }
+            this.shiftNodeDown(node);
+        }
+        var tmp=node;
+        while(tmp.parent!==null) {
+            tmp=tmp.parent;
+        }
+        this.root=tmp;
+
 		
 	}
 }
-
 var h = new MaxHeap();
-h.push(42, 15);
-h.push(14, 32);
-h.push(0, 0);
-const root = h.root;
-const left = h.root.left;
-const lastInsertedNode = h.root.right;
 
-const detached = h.detachRoot();
-h.restoreRootFromLastInsertedNode(detached);
+h.root = new Node(0, 3);
+h.root.appendChild(new Node(1, 20));
+h.root.appendChild(new Node(2, 7));
+h.root.left.appendChild(new Node(3, 5));
 
-console.log(h.parentNodes.indexOf(root)===-1);
-console.log(h.parentNodes[0]===lastInsertedNode);
-console.log(h.parentNodes[1]===left);
+/**
+ 3                        20
+ /  \                      /  \
+ 20    7  - shift down ->   5    7
+ /                          /
+ 5                          3
+ **/
+
+h.parentNodes = [
+    h.root.left,
+    h.root.right,
+    h.root.left.left,
+];
+
+const newRoot = h.root.left;
+const newDeepest = h.root;
+
+h.shiftNodeDown(h.root);
+console.log(h.root===newRoot);
+console.log(h.root.left.left===newDeepest);
 module.exports = MaxHeap;
